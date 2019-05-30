b8a6ff557d1a8b42065d93a559b9e95402e32bd8
aaorlovich <neytronik1488@gmail.com>
2019-05-30 13:20:53 +0300

    Выполнил задание 7, использовал блокирующую очередь, сделал универсально под разные объекты, не только инты написана основная кодовая база по всем заданиям, осталось документация и тесты, если понадобится рефакторинг

--- src/main/java/ru/tinkoff/orlovich/tasks/task7/ObjectsPool.java
+++ src/main/java/ru/tinkoff/orlovich/tasks/task7/ObjectsPool.java
@@ -1,78 +1,87 @@
 package ru.tinkoff.orlovich.tasks.task7;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ObjectsPool<T> {
 
-    int maxPoolSize = 5;
-    private Collection<T> objects;
-    private BlockingQueue<T> queue;
+    static Logger logger = LoggerFactory.getLogger(ObjectsPool.class);
+
+    private static BlockingQueue<ObjectFromPool> queue;
     private static ObjectsPool<?> instance = null;
 
+
     private ObjectsPool(Collection<T> objects) {
-        this.objects = objects;
+        List<ObjectFromPool> collect = objects.stream().map(s -> new ObjectFromPool(s)).collect(Collectors.toList());
+        queue = new ArrayBlockingQueue<>(objects.size(), true, collect);
     }
 
-    private ObjectsPool(int maxPoolSize, Collection<T> objects) {
-        queue = new ArrayBlockingQueue<T>(objects.size(), true, objects);
-        this.maxPoolSize = maxPoolSize;
-    }
-
-    public static <T> ObjectsPool<T> getPool(int maxPoolSize, Collection<T> objects) {
+    public static <T> ObjectsPool<T> getPool(Collection<T> objects) {
         if (instance == null) {
-            instance = new ObjectsPool<>(maxPoolSize, objects);
+            instance = new ObjectsPool<>(objects);
         }
         return (ObjectsPool<T>) instance;
     }
 
-
-    public ObjectFromPool getObjectFromPool() throws InterruptedException {
+    public ObjectFromPool<T> getObjectFromPool() throws InterruptedException {
         if (queue.isEmpty()) {
             queue.wait();
         }
-        return new ObjectFromPool(queue.take());
+        return queue.take();
     }
 
 
-    class ObjectFromPool {
+    public static class ObjectFromPool<T> {
         T value;
 
         public ObjectFromPool(T value) {
             this.value = value;
         }
 
-
         public T get() {
             return value;
         }
 
-
-        public void returnObject() {
-            queue.add(value);
-            queue.notify();
+        public void returnObjectToPool() {
+            queue.add(new ObjectFromPool(value));
+            logger.info("Object " + value.toString() + " Returned in pool!");
         }
     }
 
 
-    public static void main(String[] args) {
-        ObjectsPool<Integer> pool = ObjectsPool.getPool(10, Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    public static void main(String[] args) throws InterruptedException {
+        ObjectsPool<Integer> objPool = ObjectsPool.getPool(Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
 
-
-        Runnable runnable1 = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    pool.getObjectFromPool();
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
+        IntStream.rangeClosed(1, 40).parallel()
+                .forEach(value -> {
+                    try {
+                        ObjectFromPool<Integer> objectFromPool = objPool.getObjectFromPool();
+                        System.out.println(String.format("First test. Thread %s have unique id %s", value, objectFromPool.get()));
+                        threadImitateWork();
+                        objectFromPool.returnObjectToPool();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 
+    public static void threadImitateWork() {
+        Random random = new Random();
+        int i = random.nextInt(10);
+        try {
+            Thread.sleep(i * 1000);
+        } catch (InterruptedException e) {
+
+        }
+
+    }
 }
